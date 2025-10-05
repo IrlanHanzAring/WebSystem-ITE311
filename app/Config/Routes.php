8450d6cd762c4eb2094707de649e6ece8bd7d9e1
al287 <johnlloydalcayde870@gmail.com>
2025-10-05 20:04:45 +0800

    Added unique dashboards for it's roles

---
+++ app/Config/Routes.php
@@ -0,0 +1,25 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+/**
+ * Lab 3 ni sya dre
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/about', 'Home::about');
+$routes->get('/contact', 'Home::contact');
+/**
+ *Lab 4 ni sya dre
+ */
+$routes->get('/register', 'Auth::register');
+$routes->post('/register', 'Auth::register');
+$routes->get('/login', 'Auth::login');
+$routes->post('/login', 'Auth::login');
+$routes->get('/logout', 'Auth::logout');
+$routes->get('/dashboard', 'Auth::dashboard');
+
+$routes->setAutoRoute(true);
